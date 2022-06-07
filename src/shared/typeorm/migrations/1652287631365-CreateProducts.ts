dfa0707b9902e033f16d125e5b81ba30c86d3c83
Nilson <nilson@innovaro.com.br>
2022-06-07 17:07:14 -0300

    Fix:versions/data-type

--- src/shared/typeorm/migrations/1652287631365-CreateProducts.ts
+++ src/shared/typeorm/migrations/1652287631365-CreateProducts.ts
@@ -30,12 +30,12 @@ export class CreateProducts1652287631365 implements MigrationInterface {
                     },
                     {
                         name: 'created_at',
-                        type: 'timestamp',
+                        type: 'timestamp with time zone',
                         default: 'now()'
                     },
                     {
                         name: 'updated_at',
-                        type: 'timestamp',
+                        type: 'timestamp with time zone',
                         default: 'now()'
                     }
                 ]
