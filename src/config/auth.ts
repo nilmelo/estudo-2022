6de05e9e3408c644d7e9197abcefd53e2503001e
Nilson <nilson@innovaro.com.br>
2022-06-28 16:33:34 -0300

    feature: sessions,JWT

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: 'cb3c9137d23e8d5f0f81059352aea0ac',
+        expiresIn: '1d'
+    }
+};
--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
+import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
 
 const usersRouter = Router();
 const usersController = new UsersController();
 
-usersRouter.get('/', usersController.index);
+usersRouter.get('/', isAuthenticated, usersController.index);
 usersRouter.post('/', usersController.create);
 
 export default usersRouter;
